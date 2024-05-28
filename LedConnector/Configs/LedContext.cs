4492956a32ef60ff7c48591ede5b745ca29e0693
Sik <contact@sikelio.wtf>
2024-05-28 23:53:31 +0200

    fix(db): fixed raw message max length to 8

--- LedConnector/Configs/LedContext.cs
+++ LedConnector/Configs/LedContext.cs
@@ -50,7 +50,7 @@ namespace LedConnector.Configs
                 .Entity<Message>()
                 .Property(m => m.RawMessage)
                 .HasColumnType("varchar")
-                .HasMaxLength(9);
+                .HasMaxLength(8);
 
             builder
                 .Entity<Message>()
